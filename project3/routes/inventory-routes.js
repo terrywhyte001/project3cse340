57f0b5793e56d15b047a8855b51cc918f0d14ed2
ievbosaru <ievbosaru@byupathway.edu>
2025-09-20 17:48:01 -0700

    j

--- project3/routes/inventory-routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const inventoryController = require('../controllers/inventoryController');
-
-// Vehicle detail route
-router.get('/details/:inv_id', inventoryController.buildVehicleDetail);
-
-module.exports = router;
